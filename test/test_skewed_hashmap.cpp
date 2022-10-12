2e287a47fa6c0e19e054f248b8e5784722a76b7b
Yifan Qiao <yifanqiao@ucla.edu>
2022-10-11 17:45:08 -0700

    feat [test_skewed_hashmap]: repeat run for 100 times

--- test/test_skewed_hashmap.cpp
+++ test/test_skewed_hashmap.cpp
@@ -12,32 +12,27 @@
 #include "evacuator.hpp"
 #include "log.hpp"
 #include "sync_hashmap.hpp"
+#include "timer.hpp"
 #include "utils.hpp"
 #include "zipf.hpp"
 
-constexpr static double kZipfSkew = 0.99;
+constexpr static double kZipfSkew = 0.5;
 
-constexpr static int kNBuckets = (1 << 24);
-constexpr static int kNumMutatorThds = 20;
+constexpr static int kNBuckets = (1 << 28);
+constexpr static int kNumMutatorThds = 40;
 constexpr static int kNumGCThds = 1;
-constexpr static int kNumTotalKVPairs = 20 * 1024 * 1024;
-constexpr static int kNumOps = 2 * 1024 * 1024;
+constexpr static int kNumTotalKVPairs = 64 * 1024 * 1024;
+constexpr static int kNumOps = 4 * 1024 * 1024;
 constexpr static int kKLen = 18;
-constexpr static int kVLen = 31;
+constexpr static int kVLen = 61;
 
 constexpr static int kNumKVPairs = kNumTotalKVPairs / kNumMutatorThds;
 
-constexpr static float kSetRatio = 70;
-constexpr static float kGetRatio = 20;
-constexpr static float kRmvRatio = 10;
-
-template <int Len> struct Object;
-
-using K = Object<kKLen>;
-using V = Object<kVLen>;
-
 static std::unique_ptr<std::mt19937> mts[kNumMutatorThds];
 
+template <int Len> struct Object;
+using K = Object<kKLen>;
+using V = Object<kVLen>;
 /** Define Object */
 template <int Len> struct Object {
   char data[Len];
@@ -104,7 +99,7 @@ private:
   bool stop;
   std::shared_ptr<std::thread> evac_thd;
 
-  void init() {
+  void reset() {
     for (int i = 0; i < kNumMutatorThds; i++) {
       std::random_device rd;
       mts[i].reset(new std::mt19937(rd()));
@@ -118,14 +113,16 @@ private:
   }
 
   void prepare() {
+    nr_succ = nr_err = 0;
+
     std::vector<std::thread> thds;
     for (int tid = 0; tid < kNumMutatorThds; tid++) {
       thds.push_back(std::thread([&, tid = tid]() {
         for (int o = 0; o < kNumKVPairs; o++) {
-          K k = get_K(tid);
-          V v = get_V(tid);
-          ks[tid].push_back(k);
-          vs[tid].push_back(v);
+          const K &k = get_K(tid);
+          const V &v = get_V(tid);
+          ks[tid].emplace_back(k);
+          vs[tid].emplace_back(v);
           if (hashmap->set(k, v))
             nr_succ++;
           else
@@ -138,7 +135,7 @@ private:
     std::cout << "Finish preparation." << std::endl;
     if (nr_err > 0)
       std::cout << "Set succ " << nr_succ << ", fail " << nr_err << std::endl;
-    nr_succ = nr_err = 0;
+    // std::this_thread::sleep_for(std::chrono::seconds(5));
   }
 
   void gen_load() {
@@ -146,6 +143,7 @@ private:
     for (int tid = 0; tid < kNumMutatorThds; tid++) {
       thds.push_back(std::thread([&, tid = tid]() {
         cachebank::zipf_table_distribution<> zipf(kNumKVPairs, kZipfSkew);
+        zipf_idxes[tid].clear();
         for (int o = 0; o < kNumOps; o++) {
           auto idx = zipf(*mts[tid]);
           zipf_idxes[tid].push_back(idx);
@@ -161,20 +159,38 @@ private:
     evac_thd = std::make_shared<std::thread>([&]() {
       auto evacuator = cachebank::Evacuator::global_evacuator();
       while (!stop) {
+        auto stt = cachebank::timer::timer();
         evacuator->scan(kNumGCThds);
+        auto end = cachebank::timer::timer();
+        std::cout << "scan duration: " << cachebank::timer::duration(stt, end)
+                  << "s" << std::endl;
         // evacuator->evacuate(kNumGCThds);
         // evacuator->gc(1);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(10));
       }
     });
   }
 
 public:
-  int run() {
-    init();
-    // launch_evacuator();
+  void init() {
+    reset();
+    launch_evacuator();
     prepare();
     gen_load();
+  }
+
+  void finalize() {
+    stop = true;
+    if (evac_thd)
+      evac_thd->join();
+    evac_thd.reset();
+  }
+
+  int run() {
+    nr_succ = nr_err = 0;
+    nr_hit = nr_miss = 0;
+
+    auto stt = std::chrono::steady_clock::now();
 
     std::vector<std::thread> thds;
     for (int tid = 0; tid < kNumMutatorThds; tid++) {
@@ -193,24 +209,18 @@ public:
             ret = hashmap->set(k, v);
           }
 
-          if (!ret)
-            nr_err++;
-          else
-            nr_succ++;
+          ret ? nr_succ++ : nr_err++;
         }
       }));
     }
     for (auto &thd : thds)
       thd.join();
     thds.clear();
+    auto end = std::chrono::steady_clock::now();
 
-    std::cout << "Finish executing workload." << std::endl;
-
-    std::cout << "Cooling down..." << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(4000));
-    stop = true;
-    if (evac_thd)
-      evac_thd->join();
+    std::cout << "Finish executing workload in "
+              << std::chrono::duration<double>(end - stt).count() << "s"
+              << std::endl;
 
     std::cout << "cache hit ratio: "
               << static_cast<float>(nr_hit) / (nr_hit + nr_miss) << std::endl;
@@ -221,11 +231,18 @@ public:
       std::cout << "Get test failed! " << nr_succ << " passed, " << nr_err
                 << " failed." << std::endl;
 
+    std::cout << "Cooling down..." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+
     return 0;
   }
 };
 
 int main(int argc, char *argv[]) {
   CachebankTest test;
-  return test.run();
+  test.init();
+  for (int i = 0; i < 100; i++)
+    test.run();
+  test.finalize();
+  return 0;
 }
