15e0872ece1eb84185b27cdb604e4a4256f25bb4
Yifan Qiao <yifanqiao@ucla.edu>
2023-02-16 16:57:08 -0800

    fix [bindings/c]: should not use extern "C" in source files

--- bindings/c/resource_manager_ffi.cpp
+++ bindings/c/resource_manager_ffi.cpp
@@ -1,14 +1,6 @@
 #include "resource_manager.h"
 #include "../../inc/resource_manager.hpp"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 ResourceManager get_global_manager(void) {
   return cachebank::ResourceManager::global_manager();
 };
-
-#ifdef __cplusplus
-}
-#endif
