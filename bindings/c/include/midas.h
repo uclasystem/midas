a2684989240be6a086b1df4d8d34b229604bf438
Yifan Qiao <yifanqiao@ucla.edu>
2023-02-20 14:53:25 -0800

    fix [bindings/c]: add include/midas.h to include everything and help CMake find the include path

---
+++ bindings/c/include/midas.h
@@ -0,0 +1,17 @@
+#ifndef __MIDAS_H__
+#define __MIDAS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "cache_manager.h"
+#include "midas_utils.h"
+#include "resource_manager.h"
+#include "softmem.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MIDAS_H__
