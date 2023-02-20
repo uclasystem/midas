57ac476662a919e1ff1397582eefa1a9f6d088d0
Yifan Qiao <yifanqiao@ucla.edu>
2023-02-20 14:39:29 -0800

    fix [bindings/c]: update API usage in bindings test case

--- bindings/c/test.c
+++ bindings/c/test.c
@@ -4,7 +4,7 @@
 #include <resource_manager.h>
 
 int main(int argc, char *argv[]) {
-  ResourceManager rmanager = get_global_manager();
+  ResourceManager rmanager = midas_get_global_manager();
   printf("get midas resource manager @ %p\n", rmanager);
   sleep(1); // leave some time for rmanager to establish connections.
   return 0;
