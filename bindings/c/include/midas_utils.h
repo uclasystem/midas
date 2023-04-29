6c908a951e9a1aa894806d1927d5887eefc7252f
Yifan Qiao <yifanqiao@ucla.edu>
2023-04-28 18:09:44 -0700

    fix [bindings/c]: use `__asm__` to be compatible to c99 std

--- bindings/c/include/midas_utils.h
+++ bindings/c/include/midas_utils.h
@@ -6,13 +6,13 @@
 // high resolution timer functions
 static inline uint64_t rdtsc(void) {
   uint32_t a, d;
-  asm volatile("rdtsc" : "=a"(a), "=d"(d));
+  __asm__ volatile("rdtsc" : "=a"(a), "=d"(d));
   return ((uint64_t)a) | (((uint64_t)d) << 32);
 }
 
 static inline uint64_t rdtscp(void) {
   uint32_t a, d, c;
-  asm volatile("rdtscp" : "=a"(a), "=d"(d), "=c"(c));
+  __asm__ volatile("rdtscp" : "=a"(a), "=d"(d), "=c"(c));
   return ((uint64_t)a) | (((uint64_t)d) << 32);
 }
 
